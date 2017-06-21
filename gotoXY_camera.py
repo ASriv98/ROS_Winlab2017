ed6ba785670ec6888d9c94819164468797d41f9d
ASriv98 <atulsrivastava3@yahoo.com>
2017-06-21 12:21:21 -0400

    Add files via upload
    Updated counter clockwise/ clockwise movements, as well as a method to go straight based on a user x and y grid coordinate.

--- gotoXY_camera.py
+++ gotoXY_camera.py
@@ -64,6 +64,43 @@ def getCameraData():
     xyYawTuple = (x,y,yaw)
     return xyYawTuple
 
+def gotoPosition(x,y,distance_tolerance)
+        initial = getCameraData()
+        camera_x = initial
+
+        target_distance  = math.sqrt((x-self.pose.pose.pose.position.x)**2 + (y-self.pose.pose.pose.position.y)**2)
+        current_camera_distance = math.sqrt((initial[0])**2 + (initial[1])**2)
+        current_distance = 0
+
+    while (current_camera_distance <= target_distance)
+
+
+        while (target_distance - current_distance >= distance_tolerance):
+            #Proportional Controller
+            #linear velocity in the x-axis:
+            v_x = 1.5 * sqrt(pow((x - self.pose.pose.pose.position.x), 2) + pow((y - self.pose.pose.pose.position.y), 2))
+            if(v_x > 0.5):
+                v_x = 0.5
+            vel_msg.linear.x = v_x
+            vel_msg.linear.y = 0
+            vel_msg.linear.z = 0
+            print "target_distance:"
+            print target_distance
+            print "current_distance:"
+            print current_distance
+            #target_distance  = math.sqrt((goal_pose.pose.pose.position.x)**2 + (goal_pose.pose.pose.position.y)**2)
+            current_distance = math.sqrt((self.pose.pose.pose.position.x-current_x)**2 + (self.pose.pose.pose.position.y-current_y)**2)
+
+            #Publishing our vel_msg
+            self.velocity_publisher.publish(vel_msg)
+            self.rate.sleep()
+        #Stopping our robot after the movement is over
+        vel_msg.linear.x = 0
+        #vel_msg.angular.z = orientation - yaw
+    #(temporary for testing purposes)
+    # vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+
 class gotoXY():
 
     def __init__(self):
@@ -89,23 +126,24 @@ class gotoXY():
         goal_pose = Odometry()
         #print goal_pose
         #goal_pose.pose.pose.position.x = input("Set your x goal: ")
-      #  goal_pose.pose.pose.position.y = input("Set your y goal: ")
+       # goal_pose.pose.pose.position.y = input("Set your y goal: ")
         cur_pos = getCameraData()
         current_x = cur_pos[0]
         current_y = cur_pos[1]
         yaw_camera= cur_pos[2]
         yaw_camera = angle2pi(yaw_camera)
             #orientation = input("Set your final orientation: ")
-      #  distance_tolerance = input("Set your tolerance: ")
+        distance_tolerance = input("Set your tolerance: ")
         angle_abs = input("Set your angle: ")
-        angle_tolerance = input("Set your angle tolerance: ")
+        #angle_tolerance = input("Set your angle tolerance: ")
+        print "Goal angle: "
         print angle_abs
         vel_msg = Twist()
         
         yaw_odometry = getYaw(self)
-        print "yaw_odom:"
+        print "yaw_odom: "
         print yaw_odometry
-        print "yaw camera"
+        print "yaw camera: "
         print yaw_camera
 
         #direction = atan2(goal_pose.pose.pose.position.y-self.pose.pose.pose.position.y, goal_pose.pose.pose.position.x-self.pose.pose.pose.position.x)
@@ -114,16 +152,16 @@ class gotoXY():
         #print direction
 
         difference = angle_abs - yaw_camera
-        print "difference"
+        print "difference: "
         print difference
         angle_user = yaw_odometry + difference
-        print "angle"
+        print "angle: "
         print angle_user
         angle_user = angle2pi(angle_user)
 
         angle_tolerance = 0.1
         # sets the direction of turn
-        clockwise = False
+        """
         if abs(angle_user - yaw_camera) <= math.pi:
             if angle_user - yaw_camera > 0:
                 clockwise = True
@@ -134,6 +172,19 @@ class gotoXY():
                 clockwise = False
             else:
                 clockwise = True
+        """
+
+        if abs(angle_user) <= math.pi:
+            if yaw_camera < angle_user:
+                clockwise = False
+            else:
+                clockwise = True
+        if abs(angle_user) >= math.pi:
+            if yaw_camera < angle_user
+                clockwise = False
+            else:
+                clockwise = True
+
 
         #Turns the robot   
         while abs(yaw_odometry - angle_user) >= angle_tolerance:
@@ -176,7 +227,7 @@ class gotoXY():
             angle_user = yaw_odometry + new_turn
             angle_user = angle2pi(angle_user)
 
-            """clockwise = False
+            """clockwise = False  ###old way of determining whether clockwise or counterclockwise
             if abs(angle_user - yaw_camera) <= math.pi:
                 if angle_user - yaw_camera > 0:
                     clockwise = True
@@ -187,11 +238,16 @@ class gotoXY():
                     clockwise = False
                 else:
                     clockwise = True"""
-
-            if angle_user > yaw_camera:
-                clockwise = False
-            else: 
-                clockwise = True
+            if abs(angle_user) <= math.pi:
+                if yaw_camera < angle_user:
+                    clockwise = False
+                else:
+                    clockwise = True
+            if abs(angle_user) >= math.pi:
+                if yaw_camera < angle_user:
+                    clockwise = False
+                else:
+                    clockwise = True
 
 
             while abs(yaw_odometry - angle_user) >= angle_tolerance:
@@ -225,7 +281,10 @@ class gotoXY():
             yaw_camera = cur_pos[2]
             yaw_camera = angle2pi(yaw_camera)
             print yaw_camera
-            sleep(1)  
+            sleep(1)
+
+            gotoPosition(goal_pose.pose.pose.position.x, goal_pose.pose.pose.position.y, distance_tolerance)
+
 
 
 
