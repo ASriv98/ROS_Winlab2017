95011e46169cece829f58a748373c243a5265971
ASriv98 <atulsrivastava3@yahoo.com>
2017-06-29 12:09:42 -0400

    Add files via upload
    Final version of accuracy test we used to get data (changed all the values to radians/second) because of ROS issue with sig figs in degrees.

--- roomba_accuracy_test.py
+++ roomba_accuracy_test.py
@@ -13,7 +13,7 @@ import math
 from time import sleep
 
 velocities = [0.8,1,2,3,4.5]
-times = [1,0.75, 0.5, 0.25, 0.1]
+times = [0.1,0.25,0.5,0.75,1]
 
 rospy.init_node("roomba_accuracy_test")
 velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -58,17 +58,17 @@ def lookup_odom():
 
 def check_camera():
 
-	i = 0
-	x_old = 0
-	y_old = 0
-	yaw_old = 0
+	i = 0.0000
+	x_old = 0.0
+	y_old = 0.0
+	yaw_old = 0.0
 
-	while i <= 21:
+	while i <= 21.0000:
 
 		try:
-			(trans,rot) = listener.lookupTransform('/map', '/pioneer', rospy.Time(0))
+			(trans,rot) = listener.lookupTransform('/map', '/roomba', rospy.Time(0))
 			rate.sleep()
-			i += 1
+			i += 1.000
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			continue
 
@@ -76,16 +76,18 @@ def check_camera():
 		x = trans[0]
 		y = trans[1]
 		yaw = euler[2]
-		x_avg = (x + ((i-1)*x_old))/i
-		y_avg = (y + ((i-1)*y_old))/i
-		yaw_avg = (yaw + ((i-1)*yaw_old))/i
+
+		x_avg = float((x + ((i-1)*x_old))/i)
+		y_avg = float((y + ((i-1)*y_old))/i)
+		yaw_avg = float(yaw + ((i-1)*yaw_old))/i
 		x_old = x_avg
 		y_old = y_avg
 		yaw_old = yaw_avg
 
 	quaternion = tf.transformations.quaternion_from_euler(0.0,0.0,yaw_avg)
 	trans = (x_avg,y_avg,0.0)
-	return (trans, quaternion)
+
+	return(trans,quaternion)
 
 def runFor(time, speed):
 	v_z = speed
@@ -111,31 +113,32 @@ def lookup_battery():
 	
 def log_and_print(trial, speed, time, odom_change, camera_change, current_battery):
 	odom_euler = tf.transformations.euler_from_quaternion(odom_change[1])
-	odom_yaw = math.degrees(odom_euler[2])
+	odom_yaw = odom_euler[2]
 	camera_euler = tf.transformations.euler_from_quaternion(camera_change[1])
-	camera_yaw = math.degrees(camera_euler[2])
+	camera_yaw = camera_euler[2]
 	writer.writerow({'trial': trial, 'speed': speed, 'time': time, 'odom_change': odom_yaw, 'camera_change': camera_yaw, 'current_battery': current_battery})
 	print 'trial:' ,trial, 'speed:' ,speed, 'time:' ,time, 'odom_change:' ,odom_yaw, 'camera_change:', camera_yaw, 'current_battery', current_battery
 
-with open ('roomba_accuracy_testing_data_Final.csv', 'ab') as csvfile:
+with open ('Roomba_Accuracy_Test_Trial3.csv', 'ab') as csvfile:
 
 	fieldnames = ['trial', 'speed', 'time', 'odom_change', 'camera_change', 'current_battery']
 	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 	writer.writeheader()
 	initialize()
+	sleep(2)
 
 	for time in times:
-		print time
 
 		for velocity in velocities:
-			print velocity
 
-			for trial in range(0, 20):
+			for trial in range(0, 10):
 				runFor(time, velocity)
-				sleep(1)
-				set_transform("camFrame", "camera_final", check_camera())
+				sleep(1.5)
 				set_transform("odomFrame", "odometry_final", lookup_odom())
-				log_and_print(trial, velocity, time, get_transform("camera_initial", "camera_final"), get_transform("odometry_initial", "odometry_final"), lookup_battery())
+				set_transform("camFrame", "camera_final", check_camera())
+				log_and_print(trial, velocity, time, get_transform("odometry_initial", "odometry_final"), get_transform("camera_initial", "camera_final"), lookup_battery())
 				set_transform("camFrame", "camera_initial", get_transform("camFrame", "camera_final"))
 				set_transform("odomFrame", "odometry_initial", get_transform("odomFrame", "odometry_final"))
+				sleep(1)
+
 				#print trial
