172dcb4a2d833d81a11e7af9a180dc99f58344d2
nikunjjhaveri123 <nikunjjhaveri@gmail.com>
2017-07-31 15:22:22 -0400

    Add files via upload

--- docking_test.py
+++ docking_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from time import sleep
+from std_msgs.msg import Empty
 import tf
 from geometry_msgs.msg  import Twist
 from ca_msg import Mode
@@ -8,14 +9,14 @@ from ca_msg import Mode
 
 rospy.init_node("docking_test")
 
-dock_publisher = rospy.Publisher('dock', queue_size=10)
-undock_publisher = rospy.Publiser('undock',queue_size=10)
+dock_publisher = rospy.Publisher('dock',Empty, queue_size=10)
+undock_publisher = rospy.Publiser('undock',Empty, queue_size=10)
 velocity_publisher = rospy.Publiser('cmd_vel',Twist,queue_size=10)
 
 rate = rospy.Rate(10.0)
 
 listener = tf.TransformListener()
-
+x = Empty()
 vel_msg = Twist()
 
 count = 0
@@ -31,10 +32,10 @@ def check_mode():
 
 while not rospy.is_shutdown():
 	while not check_mode():
-		dock_publisher.publish()
+		dock_publisher.publish(x)
 		rate.sleep()
 	if check_mode() == False:
-		undock_publisher.publish()
+		undock_publisher.publish(x)
 		rate.sleep(2)
 		vel_msg.linear.x = -0.2
 		velocity_publisher.publish(vel_msg)
