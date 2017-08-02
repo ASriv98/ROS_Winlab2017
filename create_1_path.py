84e6c88d289b2c371c3eb2cf93d95dd65f86f040
ASriv98 <atulsrivastava3@yahoo.com>
2017-08-02 16:36:10 -0400

    Add files via upload

--- create_1_path.py
+++ create_1_path.py
@@ -6,14 +6,11 @@ from geometry_msgs.msg import PoseStamped
 
 rospy.init_node("create_1_path")
 
-plan_publisher = rospy.Publisher('new_plan', Path, queue_size=10)
+plan_publisher = rospy.Publisher('/create_1_waypoints', Path, queue_size=10)
 
 rate = rospy.Rate(10.0)
 
-def input_points():
-        with open('points.csv') as csvfile:
-                points_reader = csv.reader(csvfile, delimiter = ',')
-                for row in points_reader:
+
 
 def publish_plan():
 
@@ -21,16 +18,18 @@ def publish_plan():
 
 	new_plan.header.frame_id = 'map'
 
-		with open('points.csv') as csvfile:
-			points_reader = csv.reader(csvfile, delimiter = ',')
-			for row in points_reader:
-				point = PoseStamped()
-				point.pose.position.x = row[0]
-				point.pose.position.y = row[1]
-				new_plan.poses.append(point)
+	with open('way_points.csv') as csvfile:
+		points_reader = csv.reader(csvfile, delimiter = ',')
+		for row in points_reader:
+			point = PoseStamped()
+			point.pose.position.x = float(row[0])
+			point.pose.position.y = float(row[1])
+			new_plan.poses.append(point)
 
 	print "Publishing a new plan..."
 	plan_publisher.publish(new_plan)
-
-publish_plan()
+	print new_plan
+while not rospy.is_shutdown():
+	raw_input("Press enter to continue: ")
+	publish_plan()
 	
