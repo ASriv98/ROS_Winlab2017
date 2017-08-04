3433aa2b5a9b0722eedc8c6b1eb93ffdf5d8234c
native-git <mjs695@rutgers.edu>
2017-08-04 13:41:07 -0400

    Add files via upload

---
+++ csv_to_waypoint_publisher.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Path
+import csv
+from geometry_msgs.msg import PoseStamped
+
+rospy.init_node("create_1_path")
+
+plan_publisher = rospy.Publisher('/create_1_waypoints', Path, queue_size=10)
+
+rate = rospy.Rate(10.0)
+
+def publish_plan():
+
+	new_plan = Path()
+	new_plan.header.frame_id = 'map'
+
+	with open('./way_points.csv') as csvfile:
+		points_reader = csv.reader(csvfile, delimiter = ',')
+		for row in points_reader:
+			point = PoseStamped()
+			point.pose.position.x = float(row[0])
+			point.pose.position.y = float(row[1])
+			new_plan.poses.append(point)
+
+	print "Publishing a new plan..."
+	plan_publisher.publish(new_plan)
+	print new_plan
+
+while not rospy.is_shutdown():
+	raw_input("Press enter to continue: ")
+	publish_plan()
