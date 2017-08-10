e191d49d341284f3fcd4f8ab89b0e75b102abdbe
nikunjjhaveri123 <nikunjjhaveri@gmail.com>
2017-08-10 14:25:38 -0400

    Add files via upload

--- docking_test.py
+++ docking_test.py
@@ -30,18 +30,37 @@ def check_mode():
 
 	return check 
 
-while not rospy.is_shutdown():
+def docking():
 	while not check_mode():
 		dock_publisher.publish(x)
 		rate.sleep()
-	if check_mode() == False:
+		if check_mode():
+			print("Roomba has Docked")
+
+def undocking():
+	while check_mode():
 		undock_publisher.publish(x)
-		rate.sleep(2)
-		vel_msg.linear.x = -0.2
-		for i in range(0,5)
-			velocity_publisher.publish(vel_msg)
-			rate.sleep(2)
-	count+=1
-	if count >= 50:
+		rate.sleep()
+		if check_mode() == False:
+			for i in range(0,5):
+				vel_msg.linear.x = -0.2
+				velocity_publisher(vel_msg)
+				rate.sleep()
+			print("Roomba is undocked")
+
+while not rospy.is_shutdown():
+	user_dock = raw_input("Press Enter to dock or Press c to exit")
+	if user_dock == "":
+		docking()
+	if user_dock == 'c':
+		print("exiting")
 		break
+	user_dock = raw_input("Press Enter to undock or Press c to exit")
+	if user_dock == "":
+		undocking()
+	if user_dock == 'c'
+		print("exiting")
+		break
+	rate.sleep()
+
 
